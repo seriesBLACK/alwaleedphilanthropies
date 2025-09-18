78262b1d0f264b0d4b24fc08f53a75ec9759f19b
Abd ul Walee Talal <aboodtalal51@gmail.com>
2025-09-18 03:27:24 +0300

    update label text

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -164,7 +164,7 @@ export default function GrantFormClone() {
 
               {/*رثم البنك */}
               <div>
-                <label className="block mb-1">رقم البنك*</label>
+                <label className="block mb-1">رقم حسابك البنكي*</label>
                 <input name="bankNum" value={form.bankNum} onChange={handleChange} className={`w-full border rounded-xl px-4 py-2.5 ${errors.bankNum ? "border-red-400" : "border-gray-300"}`} />
                 {errors.bankNum && <p className="text-red-600 text-sm mt-1">{errors.bankNum}</p>}
               </div>
